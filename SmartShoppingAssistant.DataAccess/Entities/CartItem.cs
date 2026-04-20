aee7e8db954438c934bf7d09566d500c522b3df6
florin977 <florin.docheru@yahoo.com>
2026-04-20 11:49:31 +0300

    Initial commit on branch UserAuth

--- SmartShoppingAssistant.DataAccess/Entities/CartItem.cs
+++ SmartShoppingAssistant.DataAccess/Entities/CartItem.cs
@@ -7,6 +7,7 @@ namespace SmartShoppingAssistant.DataAccess.Entities
     public class CartItem
     {
         public int Id { get; set; }
+        public int CartId { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; } = null!;
         public int Quantity { get; set; }
