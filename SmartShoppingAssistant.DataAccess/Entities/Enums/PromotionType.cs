48f53702fdf0ccd8414590df2101601b9a876b75
florin977 <florin.docheru@yahoo.com>
2026-05-01 11:09:50 +0300

    Folder structure updated

---
+++ SmartShoppingAssistant.DataAccess/Entities/Enums/PromotionType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartShoppingAssistant.DataAccess.Entities.Enums
+{
+    internal class PromotionType
+    {
+    }
+}
---
+++ SmartShoppingAssistant.DataAccess/Repository/BaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartShoppingAssistant.DataAccess.Repository
+{
+    internal class BaseRepository
+    {
+    }
+}
