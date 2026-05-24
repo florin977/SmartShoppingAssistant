9255cd49db1097e74d491a50b0569873e772b69f
florin977 <florin.docheru@yahoo.com>
2026-05-24 19:55:17 +0300

    Requirements for the register DTO

--- SmartShoppingAssistant.BusinessLogic/DTOs/UserDTOs/UserPostDTO.cs
+++ SmartShoppingAssistant.BusinessLogic/DTOs/UserDTOs/UserPostDTO.cs
@@ -1,11 +1,20 @@
 ﻿using SmartShoppingAssistant.DataAccess.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartShoppingAssistant.BusinessLogic.DTOs.UserDTOs
 {
     public class UserPostDTO
     {
+        [Required(ErrorMessage = "Username is required.")]
+        [MinLength(3, ErrorMessage = "Username must be at least 3 characters long.")]
         public string Username { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; } = null!;
     }
 }
